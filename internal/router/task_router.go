dc45c8351b6bd1dddece3d8e6c6f85cf86ec21ac
handrixn <handrian.ch@gmail.com>
2024-02-18 14:40:24 +0700

    feature: add simple authentication for tasks endpoint - create new middleware for validate token from headers - implementing authentication middleware in tasks routes - add new key in .env.example for holding value api token

--- internal/router/task_router.go
+++ internal/router/task_router.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/handrixn/task-tracker/internal/handler"
+	"github.com/handrixn/task-tracker/internal/middleware"
 )
 
 func NewTaskRouter(r *mux.Router, h *handler.TaskHandler) *mux.Router {
 	taskRoute := r.PathPrefix("/tasks").Subrouter()
+	taskRoute.Use(middleware.ValidateAPIToken)
 
 	taskRoute.HandleFunc("/", h.TaskList).Methods(http.MethodGet)
 	taskRoute.HandleFunc("/create", h.CreateTask).Methods(http.MethodPost)
